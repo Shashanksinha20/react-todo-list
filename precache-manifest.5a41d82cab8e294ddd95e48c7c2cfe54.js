bd7d6a140c240fd620e77de358c674080fc2be7f
Shashank Sinha <shashank@Shashanks-MacBook-Pro.local>
2020-03-15 03:04:24 +0530

    Updates

---
+++ precache-manifest.5a41d82cab8e294ddd95e48c7c2cfe54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4b0dc4581564b3565d31a508b569fd",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "ec7a1e67c882c2c63dd6",
+    "url": "/react-todo-list/static/css/main.2ecb76d0.chunk.css"
+  },
+  {
+    "revision": "e2c6b16d62e469c5a26d",
+    "url": "/react-todo-list/static/js/2.5e91b57a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-todo-list/static/js/2.5e91b57a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7a1e67c882c2c63dd6",
+    "url": "/react-todo-list/static/js/main.387f2d37.chunk.js"
+  },
+  {
+    "revision": "5b4773b8471753588a75",
+    "url": "/react-todo-list/static/js/runtime-main.4ca33bd1.js"
+  }
+]);
